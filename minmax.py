9fe294f3996eca3991cdcd99f175cc9a498d718a
aravinthcheran <cheran411@gmail.com>
2024-02-08 13:43:47 +0530

    Refactor min_max function to remove unnecessary type annotations and improve readability

--- minmax.py
+++ minmax.py
@@ -3,12 +3,12 @@ from array import array
 from typing import Tuple
 
 def min_max(arr: array) -> Tuple[int, int]:
-    low: int = 0
-    high: int = len(arr) - 1
+    low= 0
+    high= len(arr) - 1
     if low == high:
         return arr[low],arr[low]
     else:
-        mid: int = (low + high) // 2
+        mid = (low + high) // 2
         min1, max1 = min_max(arr[low:mid+1])
         min2, max2 = min_max(arr[mid+1:high+1])
         return min1 if min1 < min2 else min2, max1 if max1 > max2 else max2
