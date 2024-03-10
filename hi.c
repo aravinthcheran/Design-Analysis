b3cd19820ab769f7193b307e9e1562e28602a7c8
aravinthcheran <cheran411@gmail.com>
2024-03-10 22:53:04 +0530

    Refactor merge sort and quicksort algorithms

--- hi.c
+++ hi.c
@@ -1,128 +1,21 @@
+#include <sys/types.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h> // Include header file for wait function
 
-#define MAX_TREE_HT 100
+int value = 5;
 
-// A Huffman tree node
-struct MinHeapNode {
-    char data;
-    unsigned freq;
-    struct MinHeapNode *left, *right;
-};
-
-// A Min Heap: Collection of min-heap (or Huffman tree) nodes
-struct MinHeap {
-    unsigned size;
-    unsigned capacity;
-    struct MinHeapNode **array;
-};
-
-// Function to create a new Min Heap Node
-struct MinHeapNode* newNode(char data, unsigned freq) {
-    struct MinHeapNode* temp = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
-    temp->left = temp->right = NULL;
-    temp->data = data;
-    temp->freq = freq;
-    return temp;
-}
-
-// Function to create a min heap of given capacity
-struct MinHeap* createMinHeap(unsigned capacity) {
-    struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
-    minHeap->size = 0;
-    minHeap->capacity = capacity;
-    minHeap->array = (struct MinHeapNode**)malloc(minHeap->capacity * sizeof(struct MinHeapNode*));
-    return minHeap;
-}
-
-// Function to swap two min heap nodes
-void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b) {
-    struct MinHeapNode* t = *a;
-    *a = *b;
-    *b = t;
-}
-
-// Function to heapify at given index
-void minHeapify(struct MinHeap* minHeap, int idx) {
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
-    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
-        smallest = left;
-    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
-        smallest = right;
-    if (smallest != idx) {
-        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
-        minHeapify(minHeap, smallest);
-    }
-}
-
-// Function to check if size of heap is 1 or not
-int isSizeOne(struct MinHeap* minHeap) {
-    return (minHeap->size == 1);
-}
-
-// Function to extract minimum value node from heap
-struct MinHeapNode* extractMin(struct MinHeap* minHeap) {
-    struct MinHeapNode* temp = minHeap->array[0];
-    minHeap->array[0] = minHeap->array[minHeap->size - 1];
-    --minHeap->size;
-    minHeapify(minHeap, 0);
-    return temp;
-}
-
-// Function to insert a new node to Min Heap
-void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode) {
-    ++minHeap->size;
-    int i = minHeap->size - 1;
-    while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq) {
-        minHeap->array[i] = minHeap->array[(i - 1) / 2];
-        i = (i - 1) / 2;
-    }
-    minHeap->array[i] = minHeapNode;
-}
-
-// Function to build a Huffman tree and print codes by traversing the built Huffman tree
-void buildHuffmanTree(char data[], int freq[], int size) {
-    struct MinHeapNode *left, *right, *top;
-    struct MinHeap* minHeap = createMinHeap(size);
-    for (int i = 0; i < size; ++i)
-        insertMinHeap(minHeap, newNode(data[i], freq[i]));
-    while (!isSizeOne(minHeap)) {
-        left = extractMin(minHeap);
-        right = extractMin(minHeap);
-        top = newNode('$', left->freq + right->freq);
-        top->left = left;
-        top->right = right;
-        insertMinHeap(minHeap, top);
-    }
-    printCodes(minHeap->array[0], "", 0);
-}
-
-// Function to print Huffman codes from the root of Huffman Tree
-void printCodes(struct MinHeapNode* root, char arr[], int top) {
-    if (root->left) {
-        arr[top] = '0';
-        printCodes(root->left, arr, top + 1);
-    }
-    if (root->right) {
-        arr[top] = '1';
-        printCodes(root->right, arr, top + 1);
-    }
-    if (!(root->left) && !(root->right)) {
-        printf("%c: ", root->data);
-        for (int i = 0; i < top; ++i)
-            printf("%c", arr[i]);
-        printf("\n");
-    }
-}
-
-// Main function
 int main() {
-    char arr[] = {'a', 'b', 'c', 'd', 'e', 'f'};
-    int freq[] = {5, 9, 12, 13, 16, 45};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    buildHuffmanTree(arr, freq, size);
-    return 0;
+    pid_t pid; // Corrected data type of pid_t
+    pid = fork();
+    
+    if (pid == 0) { // Child process
+        value += 15;
+        printf("CHILD: value = %d\n", value); // Output from child process
+        return 0;
+    } else if (pid > 0) { // Parent process
+        wait(NULL); // Wait for the child process to complete
+        printf("PARENT: value = %d\n", value); // Output from parent process
+        return 0;
+    }
 }
